1e988e78efdaeaf2f877e3e028fc506076f26b9a
Krystof272 <krsytof272@gmail.com>
2026-04-21 12:33:14 +0200

    change moved logic from console to MainWindow

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -3,23 +3,38 @@ import java.awt.*;
 
 public class MainWindow extends JFrame {
     private PaintLine paintLine;
+    private GameMechanics gameMechanics;
 
     public MainWindow(){
+        this.gameMechanics = new GameMechanics();
         this.paintLine = new PaintLine();
     }
 
     public void init(){
         setTitle("Trade Game");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1920, 1080);
         setLayout(new BorderLayout());
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
 
         add(paintLine, BorderLayout.CENTER);
-
+        
         setVisible(true);
+        run();
     }
 
     public void update(int y){
         paintLine.paint(y);
+        System.out.println(getHeight());
+    }
+
+    public void run(){
+        while (true){
+            update(gameMechanics.change());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
