c2371dbdf8346efe3d1155b45aafe587b61659e0
Krystof272 <krsytof272@gmail.com>
2026-04-21 13:08:55 +0200

    fix

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -2,11 +2,12 @@ import java.util.Random;
 
 public class GameMechanics {
     private final Random rnd;
+
     public GameMechanics() {
         rnd = new Random();
     }
 
     public int change(){
-        return  rnd.nextInt(-400, 400);
+        return  rnd.nextInt(900);
     }
 }
