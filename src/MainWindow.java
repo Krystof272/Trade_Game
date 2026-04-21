1a894c3461eaae646b3d91508b4c693f89acc710
Krystof272 <krsytof272@gmail.com>
2026-04-21 13:09:01 +0200

    fix

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -2,39 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
-    private PaintLine paintLine;
-    private GameMechanics gameMechanics;
 
-    public MainWindow(){
-        this.gameMechanics = new GameMechanics();
-        this.paintLine = new PaintLine();
+    public MainWindow() {
     }
 
-    public void init(){
+    public void init() {
         setTitle("Trade Game");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        add(paintLine, BorderLayout.CENTER);
-        
+        JPanel panel = new JPanel();
+
+        JButton button = new JButton("Open graph");
+
+        panel.add(button);
+
+
+        add(panel, BorderLayout.NORTH);
+
+
+        button.addActionListener(e -> {
+            dispose();
+            new TradeWindow().init();
+        });
+
+
         setVisible(true);
-        run();
-    }
-
-    public void update(int y){
-        paintLine.paint(y);
-        System.out.println(getHeight());
-    }
-
-    public void run(){
-        while (true){
-            update(gameMechanics.change());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
