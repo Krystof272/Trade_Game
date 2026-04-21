20e952868101e6acc67c49a4d25b77435182cec1
Krystof272 <krsytof272@gmail.com>
2026-04-21 13:09:39 +0200

    added logic to class

--- src/TradeWindow.java
+++ src/TradeWindow.java
@@ -1,2 +1,41 @@
-public class TradeWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class TradeWindow extends JFrame {
+    private final PaintLine paintLine;
+    private final GameMechanics gameMechanics;
+    private final ArrayList<Integer> yHistory;
+
+    public TradeWindow() {
+        this.gameMechanics = new GameMechanics();
+        this.paintLine = new PaintLine();
+        this.yHistory = new ArrayList<>();
+    }
+
+    public void init() {
+        setTitle("Trade Window");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        add(paintLine, BorderLayout.CENTER);
+
+        setVisible(true);
+
+        run();
+    }
+
+    public void update() {
+        yHistory.add(gameMechanics.change());
+        paintLine.paint(yHistory);
+    }
+
+    public void run() {
+        yHistory.add(getHeight());
+
+        Timer timer = new Timer(1000, e -> update());
+        timer.start();
+    }
+
 }
