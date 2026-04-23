8626c726ac4db7440b4d4ddc34e39b161119f893
Krystof272 <krsytof272@gmail.com>
2026-04-23 17:13:51 +0200

    changed from where to where yHistory generates

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -3,26 +3,45 @@ import java.util.Random;
 
 public class GameMechanics {
     private final Random rnd;
-    private LinkedList<Integer> yHistory;
+    private final LinkedList<Integer> yHistory;
+    private int height;
 
     public GameMechanics() {
         this.rnd = new Random();
         this.yHistory = new LinkedList<>();
+        yHistory.add(0);
+        this.height = 0;
     }
 
     public void addNextNumber() {
-        int number = yHistory.getLast() + rnd.nextInt(-200, 200);
-        if (number < 0){
-            number = 0;
+        int number = yHistory.getLast() + rnd.nextInt( 500);
+
+        if (number < yHistory.getFirst()) {
+            number = yHistory.getFirst();
         }
         yHistory.add(number);
+
+        if (number > height - 50) {
+            insreaseRatio();
+        }
     }
 
     public LinkedList<Integer> get_yHistory() {
         return yHistory;
     }
 
-    public void addTo_yHistory(int number) {
-        this.yHistory.add(number);
+    public void insreaseRatio() {
+        for (int i = 1; i < yHistory.size(); i++) {
+            int number = yHistory.get(i) / 2;
+
+            if (number < yHistory.getFirst()) {
+                number = yHistory.getFirst();
+            }
+            yHistory.set(i, number);
+        }
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
