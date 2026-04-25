efed9e363c89c0140b3f9b4d2e010759a33ca2b4
Krystof272 <krsytof272@gmail.com>
2026-04-25 19:03:29 +0200

    rename of buttons and label, moved run to MyWindow

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends MyWindow {
     private final GameMechanics gameMechanics;
-    private JButton b2;
-    private JLabel l1;
+    private JButton button2;
+    private JLabel label;
 
     public MainWindow() {
         this.gameMechanics = new GameMechanics();
-        this.b2 = new JButton("Next Week");
-        this.l1 = new JLabel("Price: ↓" + gameMechanics.get_yHistory().getLast());
+        this.button2 = new JButton("Next Week");
+        this.label = new JLabel("Price: ↓" + gameMechanics.get_yHistory().getLast());
     }
 
     public void init() {
@@ -21,73 +21,57 @@ public class MainWindow extends JFrame {
         JPanel northPanel = new JPanel();
         JPanel southPanel = new JPanel();
 
-        JPanel p1 = new JPanel();
-        p1.setLayout(new BorderLayout());
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BorderLayout());
 
-        JButton b1 = new JButton("Detailed view");
-        CustomButton.changeBlue(b1);
-        p1.add(b1, BorderLayout.SOUTH);
+        JButton button1 = new JButton("Detailed view");
+        CustomButton.changeBlue(button1);
+        panel1.add(button1, BorderLayout.SOUTH);
 
-        l1.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-        l1.setForeground(Color.RED);
-        p1.add(l1, BorderLayout.NORTH);
+        label.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        label.setForeground(Color.RED);
+        panel1.add(label, BorderLayout.NORTH);
 
 
-        northPanel.add(p1);
+        northPanel.add(panel1);
 
         add(northPanel, BorderLayout.NORTH);
 
 
-        CustomButton.changeRed(b2);
-        b2.setEnabled(false);
-
-        southPanel.add(b2);
+        CustomButton.changeGreen(button2);
+        southPanel.add(button2);
 
         add(southPanel, BorderLayout.SOUTH);
 
 
-        b1.addActionListener(e -> {
+        button1.addActionListener(e -> {
             dispose();
             new TradeWindow(gameMechanics).init();
         });
 
-        b2.addActionListener(e -> {
-            b2.setEnabled(false);
-            CustomButton.changeRed(b2);
-            run();
+        button2.addActionListener(e -> {
+            button2.setEnabled(false);
+            CustomButton.changeRed(button2);
+            run(button2);
         });
 
         setVisible(true);
-
-        run();
     }
 
-    public void run() {
-        int[] count = {0};
-        Timer timer = new Timer(1000, e -> {
-            if (count[0] < 7) {
-                update();
-                count[0]++;
-            } else {
-                ((Timer) e.getSource()).stop();
-                b2.setEnabled(true);
-                CustomButton.changeGreen(b2);
-            }
-        });
-        timer.start();
-    }
-
+    @Override
     public void update() {
         gameMechanics.addNextNumber();
+        updateText();
+    }
 
+    public void updateText() {
         int price = gameMechanics.get_yHistory().getLast();
         if (price > gameMechanics.get_yHistory().get(gameMechanics.get_yHistory().size() - 2)) {
-            l1.setForeground(Color.GREEN);
-            l1.setText("Price: ↑" + price);
+            label.setForeground(Color.GREEN);
+            label.setText("Price: ↑" + price);
         } else {
-            l1.setForeground(Color.RED);
-            l1.setText("Price: ↓" + price);
+            label.setForeground(Color.RED);
+            label.setText("Price: ↓" + price);
         }
-
     }
 }
