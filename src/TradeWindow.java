5fd3678f80eb685b2a2474d32888cb2c4890dc2b
Krystof272 <krsytof272@gmail.com>
2026-04-25 19:04:59 +0200

    rename and moved run logic

--- src/TradeWindow.java
+++ src/TradeWindow.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class TradeWindow extends JFrame {
+public class TradeWindow extends MyWindow{
     private PaintLine paintLine;
     private final GameMechanics gameMechanics;
-    private JButton b1;
+    private JButton button1;
 
     public TradeWindow(GameMechanics gameMechanics) {
         this.paintLine = new PaintLine();
         this.gameMechanics = gameMechanics;
-        this.b1 = new JButton("Next Week");
+        this.button1 = new JButton("Next Week");
     }
 
     public void init() {
@@ -23,50 +23,34 @@ public class TradeWindow extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
 
-        b1.setEnabled(false);
-        CustomButton.changeRed(b1);
-        panel.add(b1, BorderLayout.EAST);
+        CustomButton.changeGreen(button1);
+        panel.add(button1, BorderLayout.EAST);
 
-        JButton b2 = new JButton("<--");
-        CustomButton.changeRed(b2);
-        panel.add(b2, BorderLayout.WEST);
+        JButton button2 = new JButton("<--");
+        CustomButton.changeRed(button2);
+        panel.add(button2, BorderLayout.WEST);
 
         add(panel, BorderLayout.NORTH);
 
 
-        b1.addActionListener(e -> {
-            CustomButton.changeRed(b1);
-            b1.setEnabled(false);
-            run();
+        button1.addActionListener(e -> {
+            CustomButton.changeRed(button1);
+            button1.setEnabled(false);
+            run(button1);
         });
 
-        b2.addActionListener(e -> {
+        button2.addActionListener(e -> {
             dispose();
             new MainWindow().init();
         });
 
         setVisible(true);
 
-        run();
-    }
-
-    public void run() {
         gameMechanics.setHeight(getHeight());
-
-        int[] count = {0};
-        Timer timer = new Timer(1000, e -> {
-            if (count[0] < 7) {
-                update();
-                count[0]++;
-            } else {
-                ((Timer) e.getSource()).stop();
-                b1.setEnabled(true);
-                CustomButton.changeGreen(b1);
-            }
-        });
-        timer.start();
+        paintLine.paint(gameMechanics.get_yHistory());
     }
 
+    @Override
     public void update() {
         gameMechanics.addNextNumber();
         paintLine.paint(gameMechanics.get_yHistory());
