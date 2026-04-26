cd69b0811921361c682a08b67d2c587e7ee00419
Krystof272 <krsytof272@gmail.com>
2026-04-26 18:41:26 +0200

    update size

--- src/CustomButton.java
+++ src/CustomButton.java
@@ -23,7 +23,7 @@ public class CustomButton {
     public static void changeBlue(JButton button) {
         button.setBackground(new Color(33, 85, 255, 255));
         button.setForeground(Color.BLACK);
-        button.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        button.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 
         button.setFocusPainted(false);
         button.setBorderPainted(false);
