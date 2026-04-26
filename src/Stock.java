72f5917f38d31351101ff03eea130b692a67e4a6
Krystof272 <krsytof272@gmail.com>
2026-04-26 11:52:45 +0200

    added Stock.java class and variables

---
+++ src/Stock.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class Stock {
+    private String name;
+    private int fluctuation;
+    private LinkedList<Integer> numbers = new LinkedList<>();
+
+    public Stock(String name, int fluctuation) {
+        this.name = name;
+        this.fluctuation = fluctuation;
+        this.numbers = new LinkedList<>();
+        numbers.add(0);
+    }
+
+    public LinkedList<Integer> getNumbers() {
+        return numbers;
+    }
+}
