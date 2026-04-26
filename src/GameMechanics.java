dbccdbb41978a84215caf7a89086429b7b2b515a
Krystof272 <krsytof272@gmail.com>
2026-04-26 11:51:42 +0200

    initialized player and Arraylist of stocks, changed logic to work with arraylist

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -1,36 +1,42 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 
 public class GameMechanics {
     private final Random rnd;
-    private final LinkedList<Integer> yHistory;
+    private final ArrayList<Stock> stocks;
     private int height;
+    private Player player;
 
-    public GameMechanics() {
+    public GameMechanics(String name) {
         this.rnd = new Random();
-        this.yHistory = new LinkedList<>();
-        yHistory.add(0);
+        this.stocks = new ArrayList<>();
+        stocks.add(new Stock("Nvidia", 1));
         this.height = 0;
+        this.player = new Player(name);
     }
 
     public void addNextNumber() {
-        int number = yHistory.getLast() + rnd.nextInt( -100, 300);
+        int number;
+        for (Stock stock : stocks) {
+            number = stock.getNumbers().getLast() + rnd.nextInt(-100, 300);
 
-        if (number < yHistory.getFirst()) {
-            number = yHistory.getFirst();
-        }
-        yHistory.add(number);
+            if (number < stock.getNumbers().getFirst()) {
+                number = stock.getNumbers().getFirst();
+            }
+            stock.getNumbers().add(number);
 
-        if (number > height - 70) {
-            insreaseRatio();
+            if (number > height - 70) {
+                insreaseRatio(stock.getNumbers());
+            }
         }
     }
 
-    public LinkedList<Integer> get_yHistory() {
-        return yHistory;
+    public ArrayList<Stock> getStocks() {
+        return stocks;
     }
 
-    public void insreaseRatio() {
+    public void insreaseRatio(LinkedList<Integer> yHistory) {
         for (int i = 1; i < yHistory.size(); i++) {
             int number = yHistory.get(i) / 2;
 
