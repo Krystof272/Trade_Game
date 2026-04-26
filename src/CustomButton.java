b853f1d3f5dab35e06dd1f2132849868b222775c
Krystof272 <krsytof272@gmail.com>
2026-04-26 17:01:59 +0200

    change

--- src/CustomButton.java
+++ src/CustomButton.java
@@ -5,7 +5,7 @@ public class CustomButton {
     public static void changeRed(JButton button) {
         button.setBackground(new Color(255, 0, 0, 255));
         button.setForeground(Color.BLACK);
-        button.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        button.setFont(new Font("Times New Roman", Font.PLAIN, 30));
 
         button.setFocusPainted(false);
         button.setBorderPainted(false);
@@ -14,7 +14,7 @@ public class CustomButton {
     public static void changeGreen(JButton button) {
         button.setBackground(new Color(17, 255, 0, 255));
         button.setForeground(Color.BLACK);
-        button.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        button.setFont(new Font("Times New Roman", Font.PLAIN, 30));
 
         button.setFocusPainted(false);
         button.setBorderPainted(false);
