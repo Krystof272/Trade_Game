bf0b4300755dc458b65ec2ba98a3f8fe50d214f5
Krystof272 <krsytof272@gmail.com>
2026-04-26 17:05:12 +0200

    added stock panel class, logic to it

---
+++ src/StockPanel.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class StockPanel extends JPanel {
+    private JButton button;
+    private JLabel labelPrice;
+    private JLabel labelName;
+
+    public StockPanel(JButton button, JLabel labelPrice, JLabel labelName) {
+        this.button = button;
+        this.labelPrice = labelPrice;
+        this.labelName = labelName;
+    }
+
+    public void init(){
+        setLayout(new GridLayout(3, 1));
+
+        add(labelName);
+        add(labelPrice);
+        add(button);
+
+        labelPrice.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        labelName.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+
+        CustomButton.changeBlue(button);
+    }
+
+    public JButton getButton() {
+        return button;
+    }
+
+    public JLabel getLabelPrice() {
+        return labelPrice;
+    }
+}
