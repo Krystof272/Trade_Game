d7810e1bcdce51d5ca51df7f4b4697a40a52afd8
Krystof272 <krsytof272@gmail.com>
2026-04-27 14:38:57 +0200

    added methods to manage hashmap and getter, setters

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,50 @@
+import java.util.HashMap;
+
 public class Player {
     private String name;
     private int money;
+    private HashMap<String, Integer> stocksOwned;
 
     public Player(String name) {
         this.name = name;
-        this.money = 0;
+        this.money = 2000;
+        this.stocksOwned = new HashMap<>();
     }
 
+    public int getMoney() {
+        return money;
+    }
 
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void addStock(String stockName, int amount) {
+        if (stocksOwned.containsKey(stockName)) {
+            stocksOwned.replace(stockName, stocksOwned.get(stockName) + amount);
+        } else {
+            stocksOwned.put(stockName, amount);
+        }
+    }
+
+    public void substractStock(String stockName, int amount) {
+        if (stocksOwned.get(stockName) - amount > 0) {
+            stocksOwned.replace(stockName, stocksOwned.get(stockName) - amount);
+        } else {
+            stocksOwned.remove(stockName);
+        }
+    }
+
+    public boolean isStockOwned(String shareName) {
+        return stocksOwned.containsKey(shareName);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                ", stocksOwned=" + stocksOwned +
+                '}';
+    }
 }
