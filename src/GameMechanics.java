2b931e3dbc5193539345f93784ff4623fedc9a7a
Krystof272 <krystof272@gmail.com>
2026-04-28 18:43:24 +0200

    fixed bug, where price could be 0

--- src/GameMechanics.java
+++ src/GameMechanics.java
@@ -33,8 +33,8 @@ public class GameMechanics {
         for (Stock stock : stocks) {
             number = stock.getNumbers().getLast() + rnd.nextInt(-100, 200) * stock.getFluctuation();
 
-            if (number < stock.getNumbers().getFirst()) {
-                number = stock.getNumbers().getFirst();
+            if (number < 0) {
+                number = 10;
             }
             stock.getNumbers().add(number);
 
