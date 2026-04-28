3247d2838beb44b1fd7aa25aa54c7f5ff31a1370
Krystof272 <krystof272@gmail.com>
2026-04-28 18:42:51 +0200

    changed logic to work with arraylist of buttons

--- src/MyWindow.java
+++ src/MyWindow.java
@@ -1,14 +1,22 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public abstract class MyWindow extends JFrame {
     public abstract void update();
 
-    public void toDoStop(JButton button) {
-        button.setEnabled(true);
-        CustomButton.changeGreen(button);
+    public void toDoStop(ArrayList<JButton> buttons) {
+        for (JButton button : buttons) {
+            button.setEnabled(true);
+        }
+        CustomButton.changeGreen(buttons.getFirst());
     };
 
-    public void run(JButton button) {
+    public void run(ArrayList<JButton> buttons) {
+        for (JButton button : buttons) {
+            button.setEnabled(false);
+        }
+        CustomButton.changeRed(buttons.getFirst());
+
         int[] count = {0};
         Timer timer = new Timer(1000, e -> {
             if (count[0] < 7) {
@@ -16,7 +24,7 @@ public abstract class MyWindow extends JFrame {
                 count[0]++;
             } else {
                 ((Timer) e.getSource()).stop();
-                toDoStop(button);
+                toDoStop(buttons);
             }
         });
         timer.start();
