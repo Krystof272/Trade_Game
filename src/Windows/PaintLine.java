d7a8a58a12ed88cc0fd2c0b74aa7972410244bc3
Krystof272 <krsytof272@gmail.com>
2026-04-29 12:08:39 +0200

    added ratio methods

--- src/Windows/PaintLine.java
+++ src/Windows/PaintLine.java
@@ -20,47 +20,45 @@ public class PaintLine extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-
         g2d.setStroke(new BasicStroke(3));
         g2d.translate(0, getHeight() - 2);
+
+        for (int i = 0; i < 7; i++) {
+            if (yHistory.get(i) > getHeight() - 260) {
+                increaseRatio(yHistory);
+            }
+        }
 
         int x_before = 0;
         int x_now;
         int y_now;
         int y_before;
 
-        if (yHistory.size() > 7) {
-            for (int i = 0; i < 7; i++) {
-                x_now = x_before + getWidth() / 7;
-                y_now = yHistory.get(yHistory.size() - 7 + i);
-                y_before = yHistory.get(yHistory.size() - 8 + i);
+        for (int i = 0; i < 7; i++) {
+            x_now = x_before + getWidth() / 7;
+            y_now = yHistory.get(yHistory.size() - 7 + i);
+            y_before = yHistory.get(yHistory.size() - 8 + i);
 
-                if (y_now > y_before) {
-                    g2d.setColor(Color.GREEN);
-                } else {
-                    g2d.setColor(Color.RED);
-                }
-
-                g2d.drawLine(x_before, -y_before, x_now, -y_now);
-
-                x_before = x_now;
+            if (y_now > y_before) {
+                g2d.setColor(Color.GREEN);
+            } else {
+                g2d.setColor(Color.RED);
             }
-        } else {
-            for (int i = 1; i < yHistory.size(); i++) {
-                x_now = x_before + getWidth() / 7;
-                y_now = yHistory.get(i);
-                y_before = yHistory.get(i - 1);
 
-                if (y_now > y_before) {
-                    g2d.setColor(Color.GREEN);
-                } else {
-                    g2d.setColor(Color.RED);
-                }
+            g2d.drawLine(x_before, -y_before, x_now, -y_now);
 
-                g2d.drawLine(x_before, -y_before, x_now, -y_now);
+            x_before = x_now;
+        }
+    }
 
-                x_before = x_now;
+    public void increaseRatio(LinkedList<Integer> yHistory) {
+        for (int i = 1; i < yHistory.size(); i++) {
+            int number = yHistory.get(i) / 2;
+
+            if (number < 10) {
+                number = 10;
             }
+            yHistory.set(i, number);
         }
     }
 }
