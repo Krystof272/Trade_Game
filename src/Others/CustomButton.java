54a5ceda705e7037cd751fa6e992ebc983700c13
Krystof272 <krsytof272@gmail.com>
2026-05-02 23:08:09 +0200

    changed colors

--- src/Others/CustomButton.java
+++ src/Others/CustomButton.java
@@ -22,23 +22,21 @@ public class CustomButton {
         button.setBorderPainted(false);
     }
 
-    public static void changeBlue20(JButton button) {
-        button.setBackground(new Color(33, 85, 255, 255));
-        button.setForeground(Color.BLACK);
-        button.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-
-        button.setFocusPainted(false);
-        button.setBorderPainted(false);
-    }
-
-    public static void changeBlue35(JButton button) {
-        button.setBackground(new Color(33, 85, 255, 255));
-        button.setForeground(Color.BLACK);
+    public static void changeLightGray35(JButton button) {
+        button.setBackground(new Color(112, 112, 112, 255));
+        button.setForeground(Color.WHITE);
         button.setFont(new Font("Times New Roman", Font.PLAIN, 35));
 
         button.setFocusPainted(false);
         button.setBorderPainted(false);
     }
 
+    public static void changeDarkGrey20(JButton button) {
+        button.setBackground(Color.DARK_GRAY);
+        button.setForeground(Color.WHITE);
+        button.setFont(new Font("Times New Roman", Font.PLAIN, 20));
 
+        button.setFocusPainted(false);
+        button.setBorderPainted(false);
+    }
 }
