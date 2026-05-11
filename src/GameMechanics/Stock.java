55eacb4c272b08d2c8480e93672a29771c8ddaed
Krystof272 <krsytof272@gmail.com>
2026-05-11 11:47:42 +0200

    change

--- src/GameMechanics/Stock.java
+++ src/GameMechanics/Stock.java
@@ -1,21 +1,11 @@
 package GameMechanics;
 
 import java.util.LinkedList;
-import java.util.Random;
 
 public class Stock {
-    private final String name;
-    private final int fluctuation;
-    private final LinkedList<Integer> numbers ;
-
-    public Stock(String name, int fluctuation) {
-        this.name = name;
-        this.fluctuation = fluctuation;
-        this.numbers = new LinkedList<>();
-
-        Random rnd = new Random();
-        numbers.add(rnd.nextInt(500));
-    }
+    private String name;
+    private int fluctuation;
+    private LinkedList<Integer> numbers ;
 
     public LinkedList<Integer> getNumbers() {
         return numbers;
